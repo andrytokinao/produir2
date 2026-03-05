4337819a7631c0433a84e69ca05fe596fe33fe14
famajacquy <famajacquy@gmail.com>
2026-03-05 12:38:37 +0000

    change mdp

--- backoffice/db.php
+++ backoffice/db.php
@@ -5,7 +5,7 @@
 $DB_HOST = '127.0.0.1';
 $DB_NAME = 'produir2_db';
 $DB_USER = 'root';
-$DB_PASS = '';
+$DB_PASS = 'admin';
 
 try {
     $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS, [
